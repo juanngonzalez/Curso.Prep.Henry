4f8ada53e487a90e8685d5e1d40cb25dec7768f1
juanngonzalez <juan.g.gonzalez13400@gmail.com>
2021-08-12 22:18:02 -0300

    tarea resuelta

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -20,15 +20,10 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
-  let obj = {};
-
-  for(let i = 0; i < string.length; i++){
-    if(!obj[string[i]]){
-      obj[string[i]] = 0;
-    }  
-    obj[string[i]] += 1;
+  let mapaLoco = new Map()
+  for (index in string) {
+    return mapaLoco.set(string[index])
   }
-  return obj;
 }
 
 
